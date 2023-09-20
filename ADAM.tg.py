ed53345420e886fe6cc06307bc576b1a163cedae
Dobrenikie <123772484+Dobrenikie@users.noreply.github.com>
2023-09-20 18:00:27 +0300

    update

--- ADAM.tg.py
+++ ADAM.tg.py
@@ -1,7 +1,8 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow
 from tensorflow import keras
 from tensorflow.keras import layers
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 import collections
 import matplotlib.pyplot as plt
