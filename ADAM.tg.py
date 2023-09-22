986c1d1997232850df53cef1118959f0ca6ba7f4
tytyber <at2823743@gmail.com>
2023-09-22 14:23:15 +0300

    debug

--- ADAM.tg.py
+++ ADAM.tg.py
@@ -16,7 +16,8 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 from keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
-    
+
+
 
 data = pd.read_csv('Models/dataset.csv')
 np.random.seed(42)
@@ -36,24 +37,26 @@ x = pad_sequences(sequences, maxlen = maxlen)
 y = pad_sequences(y_sequences, maxlen = maxlen)
 
 
-x_train = train_test_split(x, y, test_size=0.2, random_state=42)
-x_test = train_test_split(x, y, test_size=0.2, random_state=42)
-y_train = train_test_split(x, y, test_size=0.2, random_state=42)
-y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train = train_test_split(x, test_size=0.2, random_state=42)
+x_test = train_test_split(x, test_size=0.2, random_state=42)
+y_train = train_test_split(y, test_size=0.2, random_state=42)
+y_test = train_test_split(y, test_size=0.2, random_state=42)
  
  
  
 model.add(layers.Embedding(input_dim=1500, output_dim=64))
-model.add(layers.LSTM(128, dropout = 0.2))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(128, activation='softmax'))
+model.add(layers.LSTM(428, dropout = 0.2))
+model.add(layers.Dense(120, activation='relu'))
+model.add(layers.Dense(520, activation='relu'))
+model.add(layers.Dense(120, activation='softmax'))
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-x_test = np.array(x_test, dtype=object)
-y_test = np.array(y_test, dtype=object)
+x_test = np.stack(x_train, axis=0)
+y_test = np.stack(y_train, axis=0)
+
 
 model.fit(x_train, y_train, 
            batch_size = 64,
@@ -64,7 +67,7 @@ model.fit(x_train, y_train,
 scores = model.evaluate(X_test, y_test, batch_size = 64)
 print('Точность на тестовых данных: %f' % (scores[1] * 100))
 
-# plt.plot(scores, 0)
-# plt.xlabel('X-axis')
-# plt.ylabel('Y-axis')
-# plt.show()
+plt.plot(scores, 0)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
