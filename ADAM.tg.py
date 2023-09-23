1108cf39ce4c88cbb3d768485cf5fd326ad95194
Anlixy <alex19mu9kg26@gmail.com>
2023-09-23 17:18:37 +0300

    Working version update
    РАБОТАЕТ ЕМАЕ

--- ADAM.tg.py
+++ ADAM.tg.py
@@ -1,27 +1,18 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
-import matplotlib.pyplot as plt
 from tensorflow import keras
 from keras import layers
-import collections
-import matplotlib.pyplot as plt
 import numpy as np
-from keras.preprocessing import sequence
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from matplotlib import pylab
 from keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
-from itertools import zip_longest
 
 data = pd.read_csv('Models/dataset.csv')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 np.random.seed(42)
 max_features = 1000
 maxlen = 80
-
-model = keras.Sequential()
 
 x = data['Вопрос']
 y = data['Ответ']
@@ -35,26 +26,23 @@ y = pad_sequences(y_sequences, maxlen = maxlen)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-# aligned_lists = zip_longest(x_train, y_train, fillvalue='00')
-# x_train, y_train = zip(*aligned_lists)
+model = keras.Sequential()
 
-# aligned_lists = zip_longest(x_test, y_test, fillvalue='00')
-# x_test, y_test = zip(*aligned_lists)
-
-#ИЛИ
-
-x_train = sequence.pad_sequences(x_train, maxlen = maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen = maxlen)
-
-model.add(layers.Embedding(input_dim=1000, output_dim=64))
+model.add(layers.Embedding(input_dim = max_features, output_dim=64))
 model.add(layers.LSTM(240, dropout = 0.2))
 model.add(layers.Dense(120, activation='relu'))
 model.add(layers.Dense(120, activation='softmax'))
+tf.keras.layers.Dense(1, activation="sigmoid")
 #Seq2Seq
 
-model.compile(loss = 'binary_crossentropy',
+model.compile(loss = 'sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+
+y_train = np.asarray(y_train).astype('float32').reshape((-1,1))
+y_test = np.asarray(y_test).astype('float32').reshape((-1,1))
+x_train = np.asarray(x_train).astype('float32').reshape((-1,1))
+x_test = np.asarray(x_test).astype('float32').reshape((-1,1))
 
 model.fit(x_train, y_train, 
            batch_size = 64,
@@ -62,9 +50,5 @@ model.fit(x_train, y_train,
            validation_data = (x_test, y_test),
            verbose = 1)
 
-# scores = model.evaluate(x_test, y_test, batch_size = 64)
-# print('Точность на тестовых данных: %f' % (scores[1] * 100))
-# plt.plot(scores, 0)
-# plt.xlabel('X-axis')
-# plt.ylabel('Y-axis')
-# plt.show()
+scores = model.evaluate(x_test, y_test, batch_size = 64)
+print('Точность на тестовых данных: %f' % (scores[1] * 100))
