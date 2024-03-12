8ead5e9ff052b8f73d6b2327238ba6920814a02f
Lucifer <at2823743@gmail.com>
2024-03-12 18:29:26 +0300

    debug load model

--- client/load.py
+++ client/load.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer
 import numpy as np
 import json
 import pickle
@@ -22,15 +22,15 @@ def preprocess_input_text(text, tokenizer, max_seq_len):
     return padded_sequence
 
 # Функция для получения ответа от модели
-def get_model_response(input_text, loaded_model, tokenizer, max_seq_len):
-    input_seq = tokenizer.texts_to_sequences([input_text])
-    input_padded = pad_sequences(input_seq, maxlen=max_seq_len, padding='post', truncating='post')
-    predictions = loaded_model.predict(input_padded)
+def get_model_response(input_sequence, loaded_model):
+    predictions = loaded_model.predict(input_sequence)
     predicted_index = tf.argmax(predictions, axis=-1).numpy()[0][0]  # Extract the first element
     predicted_word = tokenizer.index_word.get(predicted_index, '<OOV>')
     return predicted_word
 
 # Пример использования как чат-бота
 def answer(user_input):
-    response = get_model_response(user_input, loaded_model, tokenizer, max_seq_len)
+    input_sequence = preprocess_input_text(user_input, tokenizer, max_seq_len)
+    dummy_decoder_input = np.zeros_like(input_sequence)  # Создайте фиктивный вход для декодера
+    response = get_model_response([input_sequence, dummy_decoder_input], loaded_model)
     return response
