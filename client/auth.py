c8c301a093e126da6ca936fff9f6a856121586a8
tytyber <at2823743@gmail.com>
2024-06-03 13:12:26 +0300

    Rebuild auth code
    Переписана система авторизации (добавлена авторизация с помощью Api сайта)

--- client/auth.py
+++ client/auth.py
@@ -1,18 +1,27 @@
 import bcrypt
+import requests
+import json
 
+def authenticate_user(username, password):
+    url = 'https://atgex.ru/script/php/API.php'
+    data = {
+        'usr': username,
+        'paswd': password
+    }
 
-# Функция для подключения к базе данных MySQL
-def connect_to_database():
-    pass
-
-
-# Функция для аутентификации пользователя
-def authenticate_user(login, password):
-    pass
-
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()  # Проверка на HTTP ошибки
+        
+        if response.text.strip() == "true":
+            return True
+        else:
+            return False
+            
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return False  # Возвращаем False в случае ошибки запроса
 
 
 def is_key_valid(login, uuid):
     pass
-
-authenticate_user("admin", "admin")
